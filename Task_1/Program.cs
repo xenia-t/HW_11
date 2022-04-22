dc84225138329d6d8fccbd832927a5669e169869
Xenia <listk98@yandex.ru>
2022-04-22 23:41:03 +0300

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Определите, является ли число степенью двойки:
+   N = 16 -> "Является степень двойки"
+   N = 12 -> “Не является степенью двойки” */
+
+int N = Promt("\nВведите значение числа N: ");
+if (N >= 0)
+{
+    if (Row(N)) System.Console.WriteLine($"\nЧисло {N} является степенью двойки\n");
+    else System.Console.WriteLine($"\nЧисло {N} не является степенью двойки\n");
+}
+else System.Console.WriteLine("\nСтепенью двойки могут быть только неотрицательные числа\n");
+
+
+int Promt(String message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+bool Row (int N)
+{
+    for (int i = N; i > 1; i /= 2)
+    {
+        if (i % 2 != 0) return false;
+    }
+    return true;
+}
